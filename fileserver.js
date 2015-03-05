c3b167ffa4e00415b963047c2931dfa9b9f14f3a
Ryan Sandor Richards <ryan@runnable.com>
2015-03-05 15:56:52 -0800

    Added strong etags.

--- fileserver.js
+++ fileserver.js
@@ -14,6 +14,7 @@ var fileserver = function(app) {
     throw new Error('express app required');
   }
 
+  app.set('etag', 'strong');
   app.use(require('express-domain-middleware'));
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({
